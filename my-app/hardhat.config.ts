81f8dba0f3796ff53f4c2631df1702d837c1cadd
MSBivens <mikebiv95@gmail.com>
2022-09-21 17:38:37 -0500

    deployed publicjobboard.sol

--- my-app/hardhat.config.ts
+++ my-app/hardhat.config.ts
@@ -1,6 +1,7 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-web3";
-import "@nomiclabs/hardhat-ethers";
+// require { HardhatUserConfig } from "hardhat/config";
+require("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
 // Provide your wallet private key
 const privateKey = process.env.PRIVATE_KEY;
@@ -8,10 +9,11 @@ const privateKey = process.env.PRIVATE_KEY;
 //Provide your SKALE endpoint address
 const skale = process.env.SKALE_CHAIN_ENDPOINT;
 
-const config: HardhatUserConfig = {
+// const config: HardhatUserConfig = {
+module.exports = {
   defaultNetwork: "skale",
   solidity: {
-    version: "0.8.0",
+    version: "0.8.7",
     settings: {
       optimizer: {
         enabled: true,
@@ -22,10 +24,7 @@ const config: HardhatUserConfig = {
   networks: {
     skale: {
       url: skale,
-      accounts: privateKey,
-      gasPrice: 0,
+      accounts: [privateKey],
     },
   },
 };
-
-export default config;
---
+++ my-app/scripts/01-deploy-public-board.ts
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const PublicJobBoard = await hre.ethers.getContractFactory("PublicJobBoard");
+  const publicBoard = await PublicJobBoard.deploy();
+
+  await publicBoard.deployed();
+
+  console.log("PublicJobBoard.sol deployed to:", publicBoard.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
