c30892be946d937e556a4bb36371cf1d7221fc30
MSBivens <mikebiv95@gmail.com>
2022-09-21 01:16:30 -0500

    updated content

--- my-app/components/footer.tsx
+++ my-app/components/footer.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import packageJSON from "../package.json";
 import styles from "../styles/footer.module.css";
 
 export default function Footer() {
@@ -8,21 +7,15 @@ export default function Footer() {
       <hr />
       <ul className={styles.navItems}>
         <li className={styles.navItem}>
-          <a href="https://next-auth.js.org">Documentation</a>
+          <a href="https://github.com/MSBivens/token-boards">GitHub</a>
         </li>
         <li className={styles.navItem}>
-          <a href="https://www.npmjs.com/package/next-auth">NPM</a>
-        </li>
-        <li className={styles.navItem}>
-          <a href="https://github.com/nextauthjs/next-auth-example">GitHub</a>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/policy">
-            <a>Policy</a>
+          <Link href="/index">
+            <a>Home</a>
           </Link>
         </li>
         <li className={styles.navItem}>
-          <em>next-auth@{packageJSON.dependencies["next-auth"]}</em>
+          <em>Mike Bivens</em>
         </li>
       </ul>
     </footer>
