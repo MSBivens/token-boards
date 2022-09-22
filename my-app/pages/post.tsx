b9c461141998b6d78e34b4a6d535f4b0186503c5
MSBivens <mikebiv95@gmail.com>
2022-09-22 15:00:39 -0500

    form submission now possible

--- my-app/pages/post.tsx
+++ my-app/pages/post.tsx
@@ -1,11 +1,45 @@
 import React from "react";
 import { useState } from "react";
 import { ethers } from "ethers";
-import JobBoardABI from "../../jobBoard.json";
-import { hasEthereum, requestAccount } from "../../utils";
+import PublicJobBoard from "../PublicJobBoard.json";
+import { hasEthereum, requestAccount } from "../utils/hasEthereum";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { getCsrfToken, signIn } from "next-auth/react";
+import { SiweMessage } from "siwe";
+import { useAccount, useNetwork, useSignMessage } from "wagmi";
+import Layout from "../components/Layout";
 
 export default function post() {
+  const { signMessageAsync } = useSignMessage();
+  const { chain } = useNetwork();
+  const { address } = useAccount();
   const [loading, setLoading] = useState(false);
+
+  const handleLogin = async () => {
+    try {
+      const callbackUrl = "/protected";
+      const message = new SiweMessage({
+        domain: window.location.host,
+        address: address,
+        statement: "Sign in with Ethereum to the app.",
+        uri: window.location.origin,
+        version: "1",
+        chainId: chain?.id,
+        nonce: await getCsrfToken(),
+      });
+      const signature = await signMessageAsync({
+        message: message.prepareMessage(),
+      });
+      signIn("credentials", {
+        message: JSON.stringify(message),
+        redirect: false,
+        signature,
+        callbackUrl,
+      });
+    } catch (error) {
+      window.alert(error);
+    }
+  };
 
   const [formValues, setFormValues] = useState({
     companyName: "",
@@ -13,7 +47,7 @@ export default function post() {
     description: "",
     employmentType: "",
     location: "",
-    companyWebsiteUrl: "",
+    companyUrl: "",
     employer: "",
   });
 
@@ -36,8 +70,8 @@ export default function post() {
     if (!_formValues.location) {
       errors.location = "Location is required";
     }
-    if (!_formValues.companyWebsiteUrl) {
-      errors.companyWebsiteUrl = "Apply website URL is required";
+    if (!_formValues.companyUrl) {
+      errors.companyUrl = "Apply website URL is required";
     }
     return errors;
   };
@@ -68,120 +102,138 @@ export default function post() {
     const signer = provider.getSigner();
     const signerAddress = await signer.getAddress();
     const contract = new ethers.Contract(
-      process.env.PUBLIC_JOBBOARD_ADDRESS,
-      JobBoardABI,
+      PublicJobBoard.address,
+      PublicJobBoard.abi,
       signer
     );
 
-    let overrides = {
-      // To convert Ether to Wei:
-      value: ethers.utils.parseEther("0.005"),
-    };
-    const transaction = await contract.addJob(
+    const transaction = await contract.addPublicJob(
       formValues.companyName,
       formValues.position,
       formValues.description,
       formValues.employmentType,
       formValues.location,
-      formValues.companyWebsiteUrl,
-      overrides
+      formValues.companyUrl
     );
     await transaction.wait();
     setLoading(false);
     console.log("JOB CREATED SUCCESSFULLY");
   }
-
   return (
-    <form>
-      {formErrors.companyName && (
-        <span className="error">{formErrors.companyName}</span>
-      )}
-      <label>
-        COMPANY NAME*
-        <input
-          required
-          type="text"
-          name={Object.keys(formValues)[0]}
-          value={formValues.companyName}
-          onChange={handleOnChange}
-        ></input>
-      </label>
-
-      {formErrors.position && (
-        <span className="error">{formErrors.position}</span>
-      )}
-      <label>
-        POSITION*
-        <input
-          name={Object.keys(formValues)[1]}
-          type="text"
-          value={formValues.position}
-          onChange={handleOnChange}
-        ></input>
-      </label>
-
-      {formErrors.description && (
-        <span className="error">{formErrors.description}</span>
-      )}
-      <label>
-        DESCRIPTION*
-        <input
-          name={Object.keys(formValues)[2]}
-          type="text"
-          value={formValues.description}
-          onChange={handleOnChange}
-        ></input>
-      </label>
-
-      {formErrors.employmentType && (
-        <span className="error">{formErrors.employmentType}</span>
-      )}
-      <label>
-        EMPLOYMENT TYPE*
-        <input
-          name={Object.keys(formValues)[3]}
-          type="text"
-          value={formValues.employmentType}
-          onChange={handleOnChange}
-          placeholder="eg: Full Time"
-        ></input>
-      </label>
-
-      {formErrors.location && (
-        <span className="error">{formErrors.location}</span>
-      )}
-
-      <label>
-        LOCATION*
-        <input
-          name={Object.keys(formValues)[4]}
-          type="text"
-          value={formValues.location}
-          onChange={handleOnChange}
-        ></input>
-      </label>
-
-      {formErrors.companyWebsiteUrl && (
-        <span className="error">{formErrors.companyWebsiteUrl}</span>
-      )}
-
-      <label>
-        APPLY URL*
-        <input
-          name={Object.keys(formValues)[5]}
-          type="text"
-          value={formValues.companyWebsiteUrl}
-          onChange={handleOnChange}
-        ></input>
-      </label>
-
-      {loading ? (
-        <div className="loader"></div>
-      ) : (
-        <button onClick={(e) => handleSubmit(e, formValues)}>
-          Post your job
+    <Layout>
+      {address ? (
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            handleLogin();
+          }}
+        >
+          Sign-in
         </button>
+      ) : (
+        <ConnectButton />
       )}
-    </form>
+      <form>
+        {formErrors.companyName && (
+          <span className="error">{formErrors.companyName}</span>
+        )}
+        <label>
+          COMPANY NAME*
+          <input
+            required
+            type="text"
+            name={Object.keys(formValues)[0]}
+            value={formValues.companyName}
+            onChange={handleOnChange}
+          ></input>
+        </label>
+
+        {formErrors.position && (
+          <span className="error">{formErrors.position}</span>
+        )}
+        <label>
+          POSITION*
+          <input
+            name={Object.keys(formValues)[1]}
+            type="text"
+            value={formValues.position}
+            onChange={handleOnChange}
+          ></input>
+        </label>
+
+        {formErrors.description && (
+          <span className="error">{formErrors.description}</span>
+        )}
+        <label>
+          DESCRIPTION*
+          <input
+            name={Object.keys(formValues)[2]}
+            type="text"
+            value={formValues.description}
+            onChange={handleOnChange}
+          ></input>
+        </label>
+
+        {formErrors.employmentType && (
+          <span className="error">{formErrors.employmentType}</span>
+        )}
+        <label>
+          EMPLOYMENT TYPE*
+          <input
+            name={Object.keys(formValues)[3]}
+            type="text"
+            value={formValues.employmentType}
+            onChange={handleOnChange}
+            placeholder="eg: Full Time"
+          ></input>
+        </label>
+
+        {formErrors.location && (
+          <span className="error">{formErrors.location}</span>
+        )}
+
+        <label>
+          LOCATION*
+          <input
+            name={Object.keys(formValues)[4]}
+            type="text"
+            value={formValues.location}
+            onChange={handleOnChange}
+          ></input>
+        </label>
+
+        {formErrors.companyUrl && (
+          <span className="error">{formErrors.companyUrl}</span>
+        )}
+
+        <label>
+          APPLY URL*
+          <input
+            name={Object.keys(formValues)[5]}
+            type="text"
+            value={formValues.companyUrl}
+            onChange={handleOnChange}
+          ></input>
+        </label>
+
+        {loading ? (
+          <div className="loader"></div>
+        ) : (
+          <button onClick={(e) => handleSubmit(e, formValues)}>
+            Post your job
+          </button>
+        )}
+      </form>
+    </Layout>
   );
 }
+
+export async function getServerSideProps(context: any) {
+  return {
+    props: {
+      csrfToken: await getCsrfToken(context),
+    },
+  };
+}
+
+// Siwe.Layout = Layout;
